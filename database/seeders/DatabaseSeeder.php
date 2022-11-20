e308d2ec91f1a731dd6efa280d10c6faec797a1b
lgtkaushalya <lgtkaushalya@gmail.com>
2022-11-20 16:56:57 +0530

    Added factories to use faker to add dummy data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Post;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,6 +16,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(PostTableSeader::class);
+        //$this->call(PostTableSeader::class);
+        Post::factory(100)->create();
     }
 }
