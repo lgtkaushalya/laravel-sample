2caa0aaed13b8ce1eacf61840bbe9050d5b92772
lgtkaushalya <lgtkaushalya@gmail.com>
2022-11-21 10:49:33 +0530

    Add tailwind css

--- resources/views/fallback/index.blade.php
+++ resources/views/fallback/index.blade.php
@@ -1,1 +1,16 @@
-<h1>Fallback page once a route doesn't exist.</h1>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheeet" href="{{ asset('css/app.css') }}">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+</head>
+<body class="antialiased">
+    <div class="flex justify-center items-center h-screen">
+        <h1 class="text-3xl text-purple-600 font-bold">Laravel 9 Vite with Tailwind CSS</h1>
+    </div>
+</body>
+</html>
